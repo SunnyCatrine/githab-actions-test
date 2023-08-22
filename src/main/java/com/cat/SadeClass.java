36a73ad70fde75d704356153e0f322e3e1657e3f
Catherine <sunny4evil@gmail.com>
2023-08-22 10:53:07 +0300

    add lombok annotation to SadeClass

--- src/main/java/com/cat/SadeClass.java
+++ src/main/java/com/cat/SadeClass.java
@@ -1,6 +1,10 @@
 package com.cat;
 
+import lombok.Getter;
+
 public class SadeClass {
+    @Getter
+    private String sadeField;
     public static void main(String[] args) {
         System.out.println("Hello!!!");
     }
